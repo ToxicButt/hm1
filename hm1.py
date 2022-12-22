723c769459ba752b0d2f2bfb213f9fe4379e5cbb
ToxicButt <karkohash@mail.ru>
2022-12-22 20:54:35 +0300

    Hom1

---
+++ hm1.py
@@ -0,0 +1,12 @@
+#please_wede = input("Начертай что нибудь, бог:")
+#po4emy_net = ''.join('-' if i == ' ' else i for i in please_wede)
+#print(po4emy_net)
+
+
+#prev = input("Введи что нибудь:")
+#prev = prev.replace(" ", "-")
+#print(prev)
+
+#prev = "-".join(input("Введи что нибудь:").split())
+#print(prev)
+
